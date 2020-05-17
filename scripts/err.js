894f20b069af63e37e9967f278c1962fe1a1cdfc
Brian.cm <iles88039@gmail.com>
2020-05-17 12:20:22 +0800

    test download function.

--- scripts/err.js
+++ scripts/err.js
@@ -1,11 +1,17 @@
 var myImage = document.querySelector('img');
+var linktodownload = document.querySelector('a');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
+    
     if(mySrc === 'images/clipart4679363.png') {
       alert('Ouch! Stop poking me!');
+      linktodownload.setAttribute("href", "images/clipart4679363.png")
+      linktodownload.setAttribute("download", "clipart.png")
       myImage.setAttribute ('src',"https://developer.mozilla.org/static/img/beast-404.ce38fcf80386.png");
     } else {
+      linktodownload.removeAttribute("href")
+      linktodownload.removeAttribute("download")
       myImage.setAttribute ('src','images/clipart4679363.png');
     }
 }
